00badc93710eec42fb3499455a042d69417f4b5a
WhatJH <030226jihyun@naver.com>
2025-03-05 20:00:32 +0900

    feat: event handler 추가

--- app.js
+++ app.js
@@ -1,77 +1,81 @@
+/**
+ * 이론
+ */
+
 // alert("hi");
 
-const a = 5;
-const b = 2;
-let myName = "JH";
-// 에러가 뜨는 이유 -> const는 값으 업데이트 할 수 없으므로 에러가 뜬다.
+// const a = 5;
+// const b = 2;
+// let myName = "JH";
+// // 에러가 뜨는 이유 -> const는 값으 업데이트 할 수 없으므로 에러가 뜬다.
 
 
-console.log("Hello");
-console.log(a + b);
-console.log("Hi " + myName);
+// console.log("Hello");
+// console.log(a + b);
+// console.log("Hi " + myName);
 
-myName = "soysaouce";
+// myName = "soysaouce";
 
-console.log(" your new name is " + myName);
+// console.log(" your new name is " + myName);
 
-// let 예를 들어 이름을 바꾸고 싶으면 myName을 다시 정의해줌/ let은 새로운 것을 생성할 때 사용함.
-//  const (상수) -> 바뀔 수 없다.
-// var는 되도록 쓰지마라... 
+// // let 예를 들어 이름을 바꾸고 싶으면 myName을 다시 정의해줌/ let은 새로운 것을 생성할 때 사용함.
+// //  const (상수) -> 바뀔 수 없다.
+// // var는 되도록 쓰지마라... 
 
-// boolean : true/false
+// // boolean : true/false
 
-const ammIFat = true;
-console.log(ammIFat);
+// const ammIFat = true;
+// console.log(ammIFat);
 
-let something ;
-// 값을 주고 있지않은 상태 : undefined  컴퓨터 메모리 안에는 존재함, 공간은 존재하지만 값이 없는 상태이다.
+// let something ;
+// // 값을 주고 있지않은 상태 : undefined  컴퓨터 메모리 안에는 존재함, 공간은 존재하지만 값이 없는 상태이다.
 
-console.log(something);
+// console.log(something);
 
-// user가 로그인 했는지 t/f로 판별 가능하다
-//  null -> 아무런 값도 없다. 자연적으로 발생하는 것이 아님.
-//  파이썬에는 none, True, False로 사용함.
+// // user가 로그인 했는지 t/f로 판별 가능하다
+// //  null -> 아무런 값도 없다. 자연적으로 발생하는 것이 아님.
+// //  파이썬에는 none, True, False로 사용함.
 
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-// 효율적이 아니다 이런 리스트로 그룹화 한다면?!
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// // 효율적이 아니다 이런 리스트로 그룹화 한다면?!
 
-const daysOfWeek = mon + tue + wed  ;
-console.log(daysOfWeek)
-// 이건 그냥 문자인 상태이다.
+// const daysOfWeek = mon + tue + wed  ;
+// console.log(daysOfWeek)
+// // 이건 그냥 문자인 상태이다.
 
-const daysOfWeek2 = [mon, tue, wed ];
-const nonsense =[1, 2, "Hello",  false, null, true, undefined, "JH"]
-console.log(daysOfWeek2[1], nonsense)
-// 배열은 0부터 시작한다.
+// const daysOfWeek2 = [mon, tue, wed ];
+// const nonsense =[1, 2, "Hello",  false, null, true, undefined, "JH"]
+// console.log(daysOfWeek2[1], nonsense)
+// // 배열은 0부터 시작한다.
 
-// 추가
-daysOfWeek2.push("sun")
-console.log(daysOfWeek2)
-
-
-const apple = "apple";
-
-const toBuy = ["potato", "tomato", "pizza"]
-toBuy.push("kimchi")
-console.log(toBuy)
+// // 추가
+// daysOfWeek2.push("sun")
+// console.log(daysOfWeek2)
 
 
+// const apple = "apple";
 
-const player = {
-    name: "JH",
-    points: 10,
-    fat: true,
-}
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
-// object 뒤에 comma(,)를 사용한다 -> property가 하나 더 올 수 있기 때문.
+// const toBuy = ["potato", "tomato", "pizza"]
+// toBuy.push("kimchi")
+// console.log(toBuy)
 
-// 추가, 업데이트 가능
-player.fat = false;
-console.log(player);
+
+
+// const player = {
+//     name: "JH",
+//     points: 10,
+//     fat: true,
+// }
+// console.log(player);
+// console.log(player.name);
+// console.log(player["name"]);
+// // object 뒤에 comma(,)를 사용한다 -> property가 하나 더 올 수 있기 때문.
+
+// // 추가, 업데이트 가능
+// player.fat = false;
+// console.log(player);
 
 /**
  * function이란 뭘까?
@@ -81,25 +85,25 @@ console.log(player);
  * 
  */
 
-function sayHello (nameOfPerson, age) {
-    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
-}
+// function sayHello (nameOfPerson, age) {
+//     console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+// }
 
-sayHello("JH", 23);
-sayHello("ning ning", 26);
+// sayHello("JH", 23);
+// sayHello("ning ning", 26);
 
 // argument? function을 실행하는 동안 어떤 정보를 function에게 보낼 수 있는 방법.
 
 
-const player2 = {
-    name: "nico",
-    sayHello2: function(otherPersonsName){
-        console.log("hello! " + otherPersonsName + " nice meet you!");
-    }
-};
+// const player2 = {
+//     name: "nico",
+//     sayHello2: function(otherPersonsName){
+//         console.log("hello! " + otherPersonsName + " nice meet you!");
+//     }
+// };
 
-console.log(player2.name);
-player2.sayHello2("lynn");
+// console.log(player2.name);
+// player2.sayHello2("lynn");
 // function이 object 안에 있을 때만 가능하다.
 
 
@@ -116,7 +120,7 @@ player2.sayHello2("lynn");
 // conditional(조건문): true, false인지 알려줌
 
 // prompt는 사용자에게 입력하는 창을 띄워준다.
-const age = parseInt(prompt("How old are you?"));
+// const age = parseInt(prompt("How old are you?"));
 
 // console.log(typeof age );
 // console.log(typeof "15", typeof parseInt("15"));
@@ -149,17 +153,17 @@ const age = parseInt(prompt("How old are you?"));
  */
 
 
-if(isNaN(age) || age < 0) {
-    console.log("Please wrute a number");  
-} else if (age < 18) {
-    console.log("You are too young");
-} else if (age >= 18 && age <= 50){
-    console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-    console.log("You should exercise");
-} else if (age > 80){
-    console.log("You can do whatever you want.")
-}
+// if(isNaN(age) || age < 0) {
+//     console.log("Please wrute a number");  
+// } else if (age < 18) {
+//     console.log("You are too young");
+// } else if (age >= 18 && age <= 50){
+//     console.log("You can drink");
+// } else if (age > 50 && age <= 80) {
+//     console.log("You should exercise");
+// } else if (age > 80){
+//     console.log("You can do whatever you want.")
+// }
 // age 함수에서 string을 받아옴.
 // && 둘다 true야지 true가 됨. false가 하나라도 있으면 flase AND연산자.
 
@@ -178,3 +182,116 @@ if(isNaN(age) || age < 0) {
 // false && true === false
 // true && false === false
 // false && false === false
+
+
+/**
+ * HTML
+ */
+
+// const title = document.getElementById("title")
+
+// console.dir(title);
+
+// title.innerText = "Got you";
+
+// console.log(title.id);
+// console.log(title.className);
+
+
+/**
+ * querySelector는 element를 CSS 방식으로 검색할 수 있다.
+ * 
+ * const title = document.querySelector('#hello form');
+ * const title = document.getElementById("hello");
+ */
+
+
+/**
+ * CSS
+ */
+
+// title.style.color = "blue";
+
+/**
+ * event란?
+ * 클릭하는 것, h1위로 마우스가 올라가도 event라고 함
+ */
+
+const h1 = document.querySelector('div.hello:first-child h1');
+
+
+// function handleTitleClick(){
+//     const currnetColor = h1.style.color;
+//     let newColor;
+//     if(h1.style.color === "blue") {
+//         newColor == "tomato";
+//     } else {
+//         newColor = "blue";
+//     }
+//     h1.style.color = newColor;
+// }
+
+
+
+function handleMouserEnter(){
+    h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOnline(){
+    alert(" ALL GOOD")
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouserEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy)
+window.addEventListener("offline", handleWindowOnline);
+
+
+/**
+ * event를 사용하는 두 가지 방법
+ * 1. title.addEventListener("click", handleTItleClick); -> 2보다 많이사용
+ * 2. title.onClick = handleTitleClick;
+
+    style은 CSS에서 하자!
+    step1. element를 찾아라
+    step2. event를 listen 해라
+    step3. 그 event에 반응해라
+    반응? 무언가를 보여주거나, 감추거나, 색깔을 바꾼다는 것과 같다.
+
+ */
+
+
+// function handleTitleClick(){
+//     const clickedClass = "clikced";
+//     if (h1.className.contains(clickedClass) === clickedClass) {
+//         h1.classList.remove(clickedClass);
+//     } else {
+//         h1.classList.add(clickedClass);
+//     }
+// }
+
+
+function handleTitleClick(){
+    const clickedClass = "clikced";
+    h1.classList.toggle(clickedClass);
+}
+
+h1.addEventListener("click", handleTitleClick);
+
+
+// classList는 class들의 목록으로 작업할 수 있게끔 허용해준다.
